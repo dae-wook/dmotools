34b20d8034c8c69fe082320fd859a0e6f6bc900d
dae-wook <wkdrn8860@naver.com>
2024-08-03 17:06:02 +0900

    initial commit

---
+++ dmotools/src/main/java/com/daesoo/terracotta/DmotoolsApplication.java
@@ -0,0 +1,13 @@
+package com.daesoo.terracotta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmotoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmotoolsApplication.class, args);
+	}
+
+}
---
+++ dmotools/src/main/java/com/daesoo/terracotta/common/entity/Seal.java
@@ -0,0 +1,8 @@
+package com.daesoo.terracotta.common.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Seal {
+
+}
